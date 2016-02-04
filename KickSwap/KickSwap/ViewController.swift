5f7b3b8bb021989d2de1113e1fa5da3a84f6d603
Hugh A. Miles II <hmiles23@ufl.edu>
2016-02-03 22:57:48 -0500

    Updated .gitignore

---
+++ KickSwap/KickSwap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KickSwap
+//
+//  Created by Hugh A. Miles II on 2/3/16.
+//  Copyright © 2016 Hugh A. Miles II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
