67aeacd75be1b6747fb1a75cba03c63c84d822d2
Eric Suarez <egs104@gmail.com>
2016-02-27 16:12:39 -0500

    profile collection view working, but still need to fix user on shoe model

---
+++ KickSwap/KSProfileCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  KSProfileCollectionViewCell.swift
+//  KickSwap
+//
+//  Created by Eric Suarez on 2/27/16.
+//  Copyright © 2016 Hugh A. Miles II. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class KSProfileCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var shoeImageView: UIImageView!
+    @IBOutlet weak var shoeNameLabel: UILabel!
+    
+//    var shoe: Shoe {
+//        didSet {
+//            shoeImageView.setImageWithURL(NSURL(string: shoe.imageURL!)!)
+//            shoeNameLabel.text = shoe.name
+//        }
+//    }
+}
